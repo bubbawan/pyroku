d8c9c226b73cdda1a5cd189b33bdf22adae022a4
drwho <barth.heiko@gmail.com>
2013-09-26 16:26:37 +0200

    wsgi

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
-import os
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return 'HELLO WORLD!'
+def app(env, start_response):
+    data = 'Hello WSGI'
+    start_response('200 OK', [('Content-Type','text/plain'), ('Content-Length', str(len(data)))])
+    return iter([data])
 
